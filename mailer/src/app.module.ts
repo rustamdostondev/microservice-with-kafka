b4d0d381b9e411a110486633093efe69f14b5275
dostonbekoktamov <rustamdostondev@gmail.com>
2023-12-26 13:10:47 +0500

    add: mailer service integration with kafka

--- mailer/src/app.module.ts
+++ mailer/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigService } from './services/config.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ConfigService],
 })
 export class AppModule {}
