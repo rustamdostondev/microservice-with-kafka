e42bc9e9ba50242d6ff58acae56199c0da8bd1fe
dostonbekoktamov <rustamdostondev@gmail.com>
2023-12-26 13:57:38 +0500

    add: mailer service finished

--- mailer/src/app.module.ts
+++ mailer/src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigService } from './services/config.service';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { MailerConfigService } from './services/mailer-config.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MailerModule.forRootAsync({
+      useClass: MailerConfigService,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, ConfigService],
 })
