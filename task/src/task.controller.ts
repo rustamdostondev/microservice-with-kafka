7b19ed783eacf0c86ada240d5d2defe5d3436655
dostonbekoktamov <rustamdostondev@gmail.com>
2023-12-28 22:27:40 +0500

    add: task update pattern finished in task-service

--- task/src/task.controller.ts
+++ task/src/task.controller.ts
@@ -5,6 +5,8 @@ import { ITask } from './interfaces/task.interface';
 import { ITaskCreateResponse } from './interfaces/task-create-response.interfaces';
 import { ITaskSearchByUserResponse } from './interfaces/task-search-by-user-response.interface';
 import { ITaskDeleteResponse } from './interfaces/task-delete-response.interface';
+import { ITaskUpdateParams } from './interfaces/task-update-params.interface';
+import { ITaskUpdateByIdResponse } from './interfaces/task-update-by-id-response.interface';
 
 @Controller()
 export class TaskController {
@@ -96,6 +98,63 @@ export class TaskController {
         status: HttpStatus.BAD_REQUEST,
         message: 'task_delete_by_id_bad_request',
         errors: null,
+      };
+    }
+    return result;
+  }
+
+  @MessagePattern('task_update_by_id')
+  public async taskUpdateById(params: {
+    task: ITaskUpdateParams;
+    id: string;
+    userId: string;
+  }): Promise<ITaskUpdateByIdResponse> {
+    let result: ITaskUpdateByIdResponse;
+
+    if (params.id) {
+      try {
+        const foundTask = await this.taskService.findTaskById(params.id);
+        if (foundTask) {
+          if (foundTask.user_id === params.userId) {
+            const updateTask = Object.assign(foundTask, params.task);
+            await updateTask.save();
+
+            return {
+              status: HttpStatus.OK,
+              message: 'task_update_by_id_success',
+              task: updateTask,
+              errors: null,
+            };
+          } else {
+            result = {
+              status: HttpStatus.FORBIDDEN,
+              message: 'task_update_by_id_forbidden',
+              errors: null,
+              task: null,
+            };
+          }
+        } else {
+          result = {
+            status: HttpStatus.NOT_FOUND,
+            message: 'task_update_by_id_not_found',
+            errors: null,
+            task: null,
+          };
+        }
+      } catch (e) {
+        result = {
+          status: HttpStatus.PRECONDITION_FAILED,
+          message: 'task_update_by_id_precondition_failed',
+          errors: e.errors,
+          task: null,
+        };
+      }
+    } else {
+      result = {
+        status: HttpStatus.BAD_REQUEST,
+        message: 'task_update_by_id_bad_request',
+        errors: null,
+        task: null,
       };
     }
     return result;
