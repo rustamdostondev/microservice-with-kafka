df71b22695dac49b872a5a615dab485e49167eee
dostonbekoktamov <rustamdostondev@gmail.com>
2023-12-28 18:48:42 +0500

    add: Task schema finished

--- task/src/task.module.ts
+++ task/src/task.module.ts
@@ -2,12 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './task.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MongoConfigService } from './services/config/mongo-config.service';
+import { TaskSchema } from './schemas/task.schema';
 
 @Module({
   imports: [
     MongooseModule.forRootAsync({
       useClass: MongoConfigService,
     }),
+    MongooseModule.forFeature([
+      {
+        name: 'Task',
+        schema: TaskSchema,
+      },
+    ]),
   ],
   controllers: [AppController],
   providers: [],
