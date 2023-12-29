607d070dcee8a4d48f9a224b3dea943de641b9e7
dostonbekoktamov <rustamdostondev@gmail.com>
2023-12-29 10:53:25 +0500

    add: service and strategy finished

--- permission/src/main.ts
+++ permission/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './permission.module';
+import { PermissionModule } from './permission.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    PermissionModule,
+    {
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          brokers: ['localhost:9092'],
+          clientId: 'permission',
+        },
+        consumer: {
+          groupId: 'permission-consumer',
+        },
+      },
+    },
+  );
+  await app.listen();
 }
 bootstrap();
