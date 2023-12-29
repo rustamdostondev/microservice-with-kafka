7291b0e31e47d49060b7ecbe0434c949ba063c0e
dostonbekoktamov <rustamdostondev@gmail.com>
2023-12-29 11:01:19 +0500

    add: permisson strategy finished;

--- permission/src/services/confirmed-strategy.service.ts
+++ permission/src/services/confirmed-strategy.service.ts
@@ -1,1 +1,19 @@
-export class ConfirmedStrategyService {}
+import { IPermissionStrategy } from 'interfaces/permission-strategy.interface';
+import { IUser } from 'interfaces/user.interface';
+
+export class ConfirmedStrategyService implements IPermissionStrategy {
+  public getAllowedPermissions(user: IUser, permissions: string[]): string[] {
+    const forbiddenPermissions = [
+      'task_search_by_user_id',
+      'task_create',
+      'task_delete_by_id',
+      'task_update_by_id',
+    ];
+
+    return user.is_confirmed
+      ? permissions
+      : permissions.filter((permissions: string) => {
+          return !forbiddenPermissions.includes(permissions);
+        });
+  }
+}
