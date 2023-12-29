bdccfedb7f3d437b99e2212ae1e8225e2211855a
dostonbekoktamov <rustamdostondev@gmail.com>
2023-12-29 11:24:21 +0500

    add: permisson finished

--- permission/src/permission.controller.ts
+++ permission/src/permission.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import { Controller, HttpStatus } from '@nestjs/common';
+import { ConfirmedStrategyService } from './services/confirmed-strategy.service';
+import { MessagePattern } from '@nestjs/microservices';
+import { IUser } from 'interfaces/user.interface';
+import { IPermissionCheckResponse } from 'interfaces/permission-check-response.interface';
+import { permissions } from 'constants/permissions';
 
 @Controller()
-export class PermissionController {}
+export class PermissionController {
+  constructor(private confirmedStrategy: ConfirmedStrategyService) {}
+
+  @MessagePattern('permission_check')
+  public async permissionCheck(params: {
+    user: IUser;
+    permission: string;
+  }): Promise<IPermissionCheckResponse> {
+    let result: IPermissionCheckResponse;
+    if (!params || !params.user) {
+      result = {
+        status: HttpStatus.BAD_REQUEST,
+        message: 'permission_check_bad_request',
+        errors: null,
+      };
+    } else {
+      const allowedPermissions = this.confirmedStrategy.getAllowedPermissions(
+        params.user,
+        permissions,
+      );
+      const isAllowed = allowedPermissions.includes(params.permission);
+
+      result = {
+        status: isAllowed ? HttpStatus.OK : HttpStatus.FORBIDDEN,
+        message: isAllowed
+          ? 'permission_check_success'
+          : 'permission_check_forbidden',
+        errors: null,
+      };
+    }
+    return result;
+  }
+}
